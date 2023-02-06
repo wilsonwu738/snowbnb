f3b4759efab309e8b428c3257a45869a0edbe02f
Wilson Wu <wuyecheng738@gmail.com>
2023-02-05 19:16:55 -0500

    user model set up

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username,
+    uniqueness: true,
+    length: { in: 3..30 },
+    format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email" }
+
+  validates :email,
+    uniqueness: true,
+    length: { in: 3.. 255 },
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    save!
+    session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    while true
+      token = SecureRandom.urlsafe_base64
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+
+end
