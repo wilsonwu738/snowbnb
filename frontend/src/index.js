2188f6ee3f97dd3e67838a596c79d6343470e9bd
Wilson Wu <wuyecheng738@gmail.com>
2023-02-06 10:17:53 -0500

    frontend login form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-import csrfFetch, { restoreCSRF } from './store/csrf';
+import csrfFetch from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
   window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
 
@@ -34,10 +36,11 @@ const renderApplication = () => {
   );
 }
 
-if (sessionStorage.getItem("X-CSRF-Token") === null) {
-  restoreCSRF().then(renderApplication);
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
 } else {
   renderApplication();
 }
-
-
--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,13 +1,4 @@
-export function storeCSRFToken(response) {
-  const csrfToken = response.headers.get("X-CSRF-Token");
-  if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
-}
 
-export async function restoreCSRF() {
-  const response = await csrfFetch("/api/session");
-  storeCSRFToken(response);
-  return response;
-}
 
 
 
