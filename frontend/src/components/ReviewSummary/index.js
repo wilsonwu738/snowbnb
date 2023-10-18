2824d5c8962a6cef079efb51072759138ac130f8
Wilson Wu <wuyecheng738@gmail.com>
2023-10-17 23:11:27 -0400

    reviews summary draft 2 remove chartjs

--- frontend/src/components/ReviewSummary/index.js
+++ frontend/src/components/ReviewSummary/index.js
@@ -1,4 +1,5 @@
-import { HorizontalBar } from 'react-chartjs-2';
+import React from 'react';
+import './ReviewSummary.css';
 
 const ReviewSummary = ( { reviews }) => {
 
@@ -41,29 +42,7 @@ const ReviewSummary = ( { reviews }) => {
   const roundedOverall= Math.round(overallAverage * 100)/100
 
 
-  // Bar chart
-  const chartData = {
-    labels: ['1', '2', '3', '4', '5'],
-    datasets: [{
-      label: '# of Votes',
-      data: [ratingCounts[1], ratingCounts[2], ratingCounts[3], ratingCounts[4], ratingCounts[5]],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
+
 
   const categoryIcons = {
     communication: "📞",
@@ -77,34 +56,28 @@ const ReviewSummary = ( { reviews }) => {
 
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
-        <h1 style={{ marginRight: '10px' }}>★ {roundedOverall}</h1>
+    <div className="review-summary">
+      <div className="header">
+        <h1>★ {roundedOverall}</h1>
         <span>{numReviews} reviews</span>
       </div>
 
-      <HorizontalBar data={chartData} options={{
-        scales: {
-          x: {
-            display: false, 
-            beginAtZero: true
-          },
-          y: {
-            grid: {
-              display: false, 
-            },
-          }
-        },
-        plugins: {
-          legend: {
-            display: false, // Hide the legend
-          }
-        }
-      }} />
+      <div className="rating-bars">
+        {Object.keys(ratingCounts).sort().reverse().map(rating => (
+            <div key={rating} className="rating-bar">
+                <div 
+                    className={`rating-fill rating-${rating}`} 
+                    style={{width: `${(ratingCounts[rating] / numReviews) * 100}%`}}
+                ></div>
+            </div>
+        ))}
+      </div>
 
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', marginTop: '20px' }}>
+
+
+      <div className='categories'>
         {categories.map(category => (
-          <div key={category} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div>
             <span>{category.charAt(0).toUpperCase() + category.slice(1)}</span>
             <span>{averageRatings[category]}</span>
             <span>{categoryIcons[category]}</span>
