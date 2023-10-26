83ba904338c7c0374adcaadef23a0b2c7d3df759
Wilson Wu <wuyecheng738@gmail.com>
2023-10-25 22:43:43 -0400

    fixed review summary resize

--- frontend/src/components/ListingIndex/ListingIndexItem.js
+++ frontend/src/components/ListingIndex/ListingIndexItem.js
@@ -11,22 +11,20 @@ const ListingIndexItem = ({listing}) => {
       <Link className="listing-link" to={`/listings/${listing.id}`}>
       
         <img className='listing-photo' src={listing.photoUrl[0]} alt="" />
-        <br />
-        <br />
-        <div className="listing-location">
-          {listing.location}
-        </div>
-        <div className="listing-bedrooms">
-          {listing.numBedrooms} Bedrooms
-        </div>
-        <br />
-        <div className="listing-price">
-          ${listing.nightlyPrice} night
-        </div>
+        <div className="listing-index-info">
 
-        <br />
-        <br />
-        <br />
+          <div className="listing-location">
+            {listing.location}
+          </div>
+          <div className="listing-bedrooms">
+            {listing.numBedrooms} Bedrooms
+          </div>
+          <div className="listing-price">
+            ${listing.nightlyPrice} night
+          </div>
+
+        </div>
+    
 
 
       </Link>
