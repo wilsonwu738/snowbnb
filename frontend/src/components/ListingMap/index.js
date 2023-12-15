7c602a0a1862e7d6e35488e80f29489b3fcd74d4
Wilson Wu <wuyecheng738@gmail.com>
2023-12-15 13:23:25 -0500

    add or delete marker useEffect

--- frontend/src/components/ListingMap/index.js
+++ frontend/src/components/ListingMap/index.js
@@ -22,6 +22,57 @@ const ListingMap = ({listings}) => {
     }
   }, [mapRef, map, mapOptions]);
 
+  useEffect(() => {
+    if (map) {
+      // Add markers for new listings
+      listings.forEach((listing) => {
+        if (markers.current[listing.id]) return;
+  
+        const marker = new window.google.maps.Marker({ 
+          map, 
+          position: new window.google.maps.LatLng(listing.lat, listing.long), 
+          label: { 
+            text: `$${listing.nightlyPrice.toString()}`, 
+            fontWeight: 'bold',
+            color: 'black'
+          }, 
+          icon: {
+            path: `
+              M 1,0 
+              L 2,0 
+              A 1 1 0 0 1 3,1
+              A 1 1 0 0 1 2,2
+              L 1,2 
+              A 1 1 0 0 1 0,1
+              A 1 1 0 0 1 1,0
+              z
+            `,
+            fillOpacity: 1,
+            fillColor: 'white',
+            strokeColor: 'black',
+            strokeWeight: 1,
+            scale: 15,
+            labelOrigin: new window.google.maps.Point(1.5, 1),
+            anchor: new window.google.maps.Point(1.5, 1)
+          }, 
+        });
+
+        Object.entries(markerEventHandlers).forEach(([event, handler]) => {
+          marker.addListener(event, () => handler(listing));
+        });
+        markers.current[listing.id] = marker;
+      })
+  
+      // Remove markers for old benches
+      Object.entries(markers.current).forEach(([listingId, marker]) => {
+        if (listings.some(listing => listing.id.toString() === listingId)) return;
+        
+        marker.setMap(null);
+        delete markers.current[listingId];
+      })
+    }
+  }, [benches, history, map, markerEventHandlers]);
+
 
   return (
     <div ref={mapRef}> Map
