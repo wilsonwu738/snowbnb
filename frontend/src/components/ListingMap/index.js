8bc05bd0d2a63177e6bda1240c549b610e1a47c3
Wilson Wu <wuyecheng738@gmail.com>
2023-12-15 12:41:53 -0500

    listing map set up

--- frontend/src/components/ListingMap/index.js
+++ frontend/src/components/ListingMap/index.js
@@ -1,18 +1,40 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Wrapper } from '@googlemaps/react-wrapper';
+
+const ListingMap = ({listings}) => {
+  const [map, setMap] = useState(null);
+  const mapRef = useRef(null);
+  const markers = useRef({});
 
 
-const ListingMap = () => {
+  // Create the map
+  useEffect(() => {
+    if (!map) {
+      setMap(new window.google.maps.Map(mapRef.current, {
+        center: {
+          lat: 37.773972,
+          lng: -122.431297
+        }, // San Francisco coordinates
+        zoom: 13,
+        clickableIcons: false,
+        ...mapOptions,
+      }));
+    }
+  }, [mapRef, map, mapOptions]);
+
+
   return (
-    <div>
+    <div ref={mapRef}> Map
 
     </div>
   )
 }
 
-const ListingMapWrapper = () => {
+const ListingMapWrapper = (props) => {
   return (
-    <div>
-      
-    </div>
+    <Wrapper apiKey={ProcessingInstruction.env.REACT_APP_MAPS_API_KEY}>
+      <ListingMap {...props} />
+    </Wrapper>
   )
 }
 
