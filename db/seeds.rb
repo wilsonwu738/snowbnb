86bc03c20fc8296ebdd4a42f66cd89f0337e71ae
Wilson Wu <wuyecheng738@gmail.com>
2024-01-05 16:00:47 -0500

    added back applicationtransaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require "open-uri"
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# ApplicationRecord.transaction do 
+ApplicationRecord.transaction do 
   puts "Destroying tables..."
   # Unnecessary if using `rails db:seed:replant`
   Review.destroy_all
@@ -467,5 +467,5 @@ require "open-uri"
 
 
   puts "Done!"
-# end
+end
 
